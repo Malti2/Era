f85d3c6e0ebf735f91179cae61250f4d64696c0d
Malte Höpers <181262855+Malti2@users.noreply.github.com>
2026-07-27 19:45:05 +0200

    Update Package.swift for Era iOS app

--- Package.swift
+++ Package.swift
@@ -1,16 +1,32 @@
-// swift-tools-version: 6.0
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "Era",
-    platforms: [.iOS(.v18)],
+    platforms: [.iOS(.v17), .macOS(.v14)],
     products: [
-        .executable(name: "Era", targets: ["Era"])
-    ],
-    targets: [
-        .executableTarget(
+        .iOSApplication(
             name: "Era",
-            path: "Sources/Era"
+            targets: ["Era"],
+            bundleIdentifier: "com.malti2.era",
+            teamIdentifier: "YOUR_TEAM_ID",
+            displayVersion: "1.0",
+            bundleVersion: "1",
+            appCategory: .music,
+            supportedDeviceFamilies: [.phone, .pad],
+            buildSettings: .init(
+                infoPlist: [
+                    "UISupportedInterfaceOrientations": ["UIInterfaceOrientationPortrait"]
+                ]
+            )
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Era",
+            path: "Sources/Era",
+            resources: []
         )
     ]
 )
