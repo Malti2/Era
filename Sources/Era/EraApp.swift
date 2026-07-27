88023dee0bbb9912bab3789165fc1a8a0417f95a
Malte Höpers <181262855+Malti2@users.noreply.github.com>
2026-07-27 19:36:53 +0200

    Update EraApp with SwiftData container

--- Sources/Era/EraApp.swift
+++ Sources/Era/EraApp.swift
@@ -1,10 +1,12 @@
 import SwiftUI
+import SwiftData
 
 @main
 struct EraApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.$modelContext, ModelContainer(defaultSchema: Schema([Track.self, Tag.self, Preset.self])).modelContext)
         }
     }
 }
