3dc575fcd316a0ae19ee44780f6c2a7164639fae
Malte Höpers <181262855+Malti2@users.noreply.github.com>
2026-07-27 18:40:14 +0200

    Add Package.swift for Swift package

---
+++ Package.swift
@@ -0,0 +1,20 @@
+import PackageDescription
+
+let package = Package(
+    name: "Era",
+    platforms: [.iOS(.v18)],
+    products: [
+        .library(
+            name: "Era",
+            targets: ["Era"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "Era",
+            dependencies: [],
+            path: ".",
+            exclude: [".github"]
+        )
+    ]
+)
