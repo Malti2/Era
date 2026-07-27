b9b4476c406c040005813f7e6f479fb3fa3019d7
Malte Höpers <181262855+Malti2@users.noreply.github.com>
2026-07-27 18:50:19 +0200

    Change target to executableTarget

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         )
     ],
     targets: [
-        .target(
+        .executableTarget(
             name: "Era",
             dependencies: [],
             path: ".",
